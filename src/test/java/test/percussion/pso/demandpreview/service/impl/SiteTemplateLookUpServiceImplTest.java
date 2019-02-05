cb4322f9f4a0b4e0a8403bf406494d7115697e8a
natechadwick <nate.chadwick@gmail.com>
2019-02-05 15:09:38 -0500

    Mavenize project update for latest patch level

---
+++ src/test/java/test/percussion/pso/demandpreview/service/impl/SiteTemplateLookUpServiceImplTest.java
@@ -0,0 +1,261 @@
+package test.percussion.pso.demandpreview.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.percussion.pso.demandpreview.exception.SiteLookUpException;
+import com.percussion.pso.demandpreview.service.SiteEditionConfig;
+import com.percussion.pso.demandpreview.service.SiteEditionHolder;
+import com.percussion.pso.demandpreview.service.SiteEditionLookUpService;
+import com.percussion.pso.demandpreview.service.impl.SiteEditionLookUpServiceImpl;
+import com.percussion.services.assembly.IPSAssemblyService;
+import com.percussion.services.assembly.IPSAssemblyTemplate;
+import com.percussion.services.assembly.PSAssemblyException;
+import com.percussion.services.catalog.PSTypeEnum;
+import com.percussion.services.error.PSNotFoundException;
+import com.percussion.services.guidmgr.IPSGuidManager;
+import com.percussion.services.publisher.IPSEdition;
+import com.percussion.services.publisher.IPSPublisherService;
+import com.percussion.services.sitemgr.IPSSite;
+import com.percussion.services.sitemgr.IPSSiteManager;
+import com.percussion.utils.guid.IPSGuid;
+
+import junit.framework.Assert;
+
+import static org.junit.Assert.*;
+
+public class SiteTemplateLookUpServiceImplTest
+{
+	private static Log log = LogFactory.getLog(SiteTemplateLookUpServiceImplTest.class);
+	
+	Mockery context;
+	TestLookUpService lookUp;
+	
+	IPSSiteManager siteManager;
+	IPSPublisherService publisherService;
+	IPSAssemblyService asm; 
+	SiteEditionLookUpService siteEditionLookUpService;
+	
+	IPSGuidManager guidManager;
+
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		context = new Mockery();
+		lookUp = new TestLookUpService();
+		siteManager = context.mock(IPSSiteManager.class,"siteManager");
+		publisherService = context.mock(IPSPublisherService.class,"publisherService");
+		siteEditionLookUpService = context.mock(SiteEditionLookUpService.class,"siteEditionLookupService");
+		guidManager = context.mock(IPSGuidManager.class,"guidManager");
+		asm = context.mock(IPSAssemblyService.class,"asm");
+		lookUp.setSiteManager(siteManager);
+		lookUp.setPubisherService(publisherService);
+		lookUp.setGuidManager(guidManager);
+		lookUp.setAsm(asm); 
+	}
+
+	@Test
+	@Ignore
+	//TODO: Fix testLookUpSiteEdition
+	public void testLookUpSiteEdition() throws SiteLookUpException
+	{
+		Map<String, SiteEditionConfig> siteLookUpMap = new HashMap<String, SiteEditionConfig>();
+		SiteEditionConfig sConfig = new SiteEditionConfig();
+		sConfig.setSiteName("psoSite");
+		sConfig.setEditionName("psoEdition"); 
+		sConfig.setAssemblyContext(1); 
+		sConfig.setContextURLRootVar("http://test/");
+		siteLookUpMap.put("psoSite", sConfig);
+		lookUp.setSiteLookUpMap(siteLookUpMap);
+		
+		final String siteId = "234";
+		final SiteEditionHolder stHolder = new SiteEditionHolder();
+		final IPSSite site = context.mock(IPSSite.class,"site");
+		final IPSSite pSite = context.mock(IPSSite.class,"pSite");
+		final IPSEdition edition = context.mock(IPSEdition.class,"edition");
+		final IPSGuid siteGuid = context.mock(IPSGuid.class,"siteGuid");
+		final IPSAssemblyTemplate template = context.mock(IPSAssemblyTemplate.class,"template");
+		stHolder.setSite(pSite);
+		stHolder.setEdition(edition);
+		try
+		{
+		   context.checking(new Expectations(){{
+			one(guidManager).makeGuid(Integer.parseInt(siteId), PSTypeEnum.SITE);
+			will(returnValue(siteGuid));
+			one(siteManager).loadSite(siteGuid);
+			will(returnValue(site));
+			atLeast(1).of(site).getName();
+			will(returnValue("psoSite"));
+			one(publisherService).findEditionByName("psoEdition");
+			will(returnValue(edition));		
+			one(siteManager).loadSite("psoSite"); 
+			will(returnValue(pSite));
+		   }});	   
+		}
+		catch (SiteLookUpException ex)
+		{
+			log.error("Error looking up site info", ex);
+			fail("Exception"); 
+		} 
+		
+		SiteEditionHolder holder = lookUp.LookUpSiteEdition(siteId);
+		assertNotNull(holder);
+		assertSame(holder.getClass(), stHolder.getClass());
+	    context.assertIsSatisfied();
+	}
+	
+	@Test
+	public void testLookupWithWrongSite() throws SiteLookUpException
+	{
+		Map<String, SiteEditionConfig> siteLookUpMap = new HashMap<String, SiteEditionConfig>();
+		SiteEditionConfig sConfig = new SiteEditionConfig();
+		sConfig.setSiteName("psoSite");
+		sConfig.setEditionName("psoTemplate");
+		siteLookUpMap.put("siteName", sConfig);
+		lookUp.setSiteLookUpMap(siteLookUpMap);
+		
+		final String siteId = "234";
+		final SiteEditionHolder stHolder = new SiteEditionHolder();
+		final IPSSite site = context.mock(IPSSite.class,"site");
+		final IPSSite pSite = context.mock(IPSSite.class,"pSite");
+		final IPSEdition edition = context.mock(IPSEdition.class,"edition");
+		final IPSGuid siteGuid = context.mock(IPSGuid.class,"siteGuid");
+		stHolder.setSite(pSite);
+		stHolder.setEdition(edition);
+		try
+		{
+		   context.checking(new Expectations(){{
+		      one(guidManager).makeGuid(142, PSTypeEnum.SITE);
+	            will(returnValue(siteGuid));
+	            one(siteManager).loadSite(siteGuid);
+	            will(throwException(new PSNotFoundException("Site Not Found")));
+	     	   }});	   
+		   lookUp.LookUpSiteEdition("142");
+           Assert.fail("Test with wrong site name failed");
+		}
+		catch (SiteLookUpException ex)
+		{
+			log.info("Got expected exception " + ex, ex);
+		}
+		context.assertIsSatisfied();
+
+	}
+	
+	@Test
+	public void testLookWithNoSiteName() throws SiteLookUpException
+	{
+		Map<String, SiteEditionConfig> siteLookUpMap = new HashMap<String, SiteEditionConfig>();
+		SiteEditionConfig sConfig = new SiteEditionConfig();
+		sConfig.setSiteName("");
+		sConfig.setEditionName("psoTemplate");
+		siteLookUpMap.put("site", sConfig);
+		lookUp.setSiteLookUpMap(siteLookUpMap);
+		
+		final String siteId = "234";
+		final SiteEditionHolder stHolder = new SiteEditionHolder();
+		final IPSSite site = context.mock(IPSSite.class,"site");
+		final IPSSite pSite = context.mock(IPSSite.class,"pSite");
+		final IPSEdition edition = context.mock(IPSEdition.class,"edition");
+		final IPSGuid siteGuid = context.mock(IPSGuid.class,"siteGuid");
+		stHolder.setSite(pSite);
+		stHolder.setEdition(edition);
+		try
+		{
+		   context.checking(new Expectations(){{
+		      one(guidManager).makeGuid(Integer.parseInt(siteId), PSTypeEnum.SITE);
+		      will(returnValue(siteGuid));		
+		      one(siteManager).loadSite(siteGuid);
+		      will(returnValue(site));
+		      atLeast(1).of(site).getName();
+		      will(returnValue("siteName"));
+		   }});	   
+
+		   lookUp.LookUpSiteEdition(siteId);
+		   fail("Test with empty site name failed");
+		}
+		catch (SiteLookUpException ex)
+		{
+			log.error("Error looking up site info", ex);
+		}
+		context.assertIsSatisfied();
+
+	}
+	
+	@Test
+	public void testLookWithNoSiteEdition() throws SiteLookUpException
+	{
+		Map<String, SiteEditionConfig> siteLookUpMap = new HashMap<String, SiteEditionConfig>();
+		SiteEditionConfig sConfig = new SiteEditionConfig();
+		sConfig.setSiteName("psoSite");
+		sConfig.setEditionName("");
+		siteLookUpMap.put("site", sConfig);
+		lookUp.setSiteLookUpMap(siteLookUpMap);
+		
+		final String siteId = "234";
+		final SiteEditionHolder stHolder = new SiteEditionHolder();
+		final IPSSite site = context.mock(IPSSite.class,"site");
+		final IPSSite pSite = context.mock(IPSSite.class,"pSite");
+		final IPSEdition edition = context.mock(IPSEdition.class,"edition");
+		final IPSGuid siteGuid = context.mock(IPSGuid.class,"siteGuid");
+		stHolder.setSite(pSite);
+		stHolder.setEdition(edition);
+		try
+		{
+		   context.checking(new Expectations(){{
+			one(guidManager).makeGuid(Integer.parseInt(siteId), PSTypeEnum.SITE);
+			will(returnValue(siteGuid));		
+			one(siteManager).loadSite(siteGuid);
+            will(returnValue(site));
+			atLeast(1).of(site).getName();
+	        will(returnValue("siteName"));	        
+		   }});
+		   
+		   lookUp.LookUpSiteEdition(siteId);
+           fail("Test with empty edition Name failed");
+		}
+		catch (SiteLookUpException ex)
+		{
+			log.error("Error looking up site info", ex);
+		}
+	    context.assertIsSatisfied(); 
+
+	}	
+	
+	private class TestLookUpService extends SiteEditionLookUpServiceImpl
+	{
+		@Override
+		public void setSiteManager(IPSSiteManager siteManager)
+		{
+			super.setSiteManager(siteManager);
+		}
+		@Override
+		public void setPubisherService(IPSPublisherService publisherService)
+		{
+			super.setPubisherService(publisherService);
+		}
+		@Override
+		public void setGuidManager(IPSGuidManager guidManager)
+		{
+			super.setGuidManager(guidManager);
+		}
+      /**
+       * @see com.percussion.pso.demandpreview.service.impl.SiteEditionLookUpServiceImpl#setAsm(com.percussion.services.assembly.IPSAssemblyService)
+       */
+      @Override
+      public void setAsm(IPSAssemblyService asm)
+      {
+          super.setAsm(asm);
+      }
+		
+	}
+
+}
